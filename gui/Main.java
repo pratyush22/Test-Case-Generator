dc556423a031be9305c03363697b214e40800905
Pratyush Sharma <pratyush22@users.noreply.github.com>
2014-09-21 20:39:27 +0530

    Create Main.java

---
+++ gui/Main.java
@@ -0,0 +1,20 @@
+package gui;
+
+import javax.swing.UIManager;
+
+public class Main
+{
+	public static void main(String args[])
+	{
+		try
+		{
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+
+		new MainWindow();
+	}
+}
